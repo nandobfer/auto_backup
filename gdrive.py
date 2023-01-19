89414d63d4090baf0b91a85153e16c9f2ffab390
Burgos <nandobfer@yahoo.com.br>
2023-01-19 11:50:49 -0300

    feat: added drive upload

--- gdrive.py
+++ gdrive.py
@@ -2,15 +2,33 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 gauth = GoogleAuth()   
-gauth.LocalWebserverAuth()    
-drive = GoogleDrive(gauth)  
 
-# upload_file = 'teste.txt'
-# gfile = drive.CreateFile({'parents': [{'id': '1pzschX3uMbxU0lB5WZ6IlEEeAUE8MZ-t'}]})
-# # Read file and set it as the content of this instance.
-# gfile.SetContentFile(upload_file)
-# gfile.Upload() # Upload the file.
+# Try to load saved client credentials
+gauth.LoadCredentialsFile("mycreds.txt")
 
-file_list = drive.ListFile({'q': "'{}' in parents and trashed=false".format('1cIMiqUDUNldxO6Nl-KVuS9SV-cWi9WLi')}).GetList()
-for file in file_list:
-	print('title: %s, id: %s' % (file['title'], file['id']))
+if gauth.credentials is None:
+    # Authenticate if they're not there
+
+    # This is what solved the issues:
+    gauth.GetFlow()
+    gauth.flow.params.update({'access_type': 'offline'})
+    gauth.flow.params.update({'approval_prompt': 'force'})
+
+    gauth.LocalWebserverAuth()
+
+elif gauth.access_token_expired:
+
+    # Refresh them if expired
+
+    gauth.Refresh()
+else:
+
+    # Initialize the saved creds
+
+    gauth.Authorize()
+
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("mycreds.txt")  
+
+drive = GoogleDrive(gauth)
+
