bc5a5004ac8299d28de9e650b79bc8df489341ee
nandobfer <nandobfer@yahoo.com.br>
2023-01-19 15:03:22 -0300

    update: pydrive2

--- gdrive.py
+++ gdrive.py
@@ -1,5 +1,5 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 from datetime import datetime
 
 def upload(user, extension):
