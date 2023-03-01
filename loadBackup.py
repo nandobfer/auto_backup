ea57900913c01001833087fcaf07ff8e1da0bab9
nandobfer <nandobfer@yahoo.com.br>
2023-03-01 20:12:50 -0300

    feat: added delete file after restoring back

--- loadBackup.py
+++ loadBackup.py
@@ -4,7 +4,7 @@ from gdrive import download
 def delete(user):
     proc = subprocess.Popen([f"rm -rf {user}.tar.gz"], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
-    print("file deleted")
+    print("backup file deleted")
 
 def load(user):
     print(f'restoring {user}')
@@ -16,3 +16,4 @@ user = sys.argv[1]
 
 download(user)
 load(user)
+delete(user)
