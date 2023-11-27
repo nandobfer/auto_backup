ddae8acdbddf412237fbaa70d428e41e90f025ae
nandobfer <nandobfer@yahoo.com.br>
2023-11-27 13:25:04 -0300

    update: printing website

--- backallup.py
+++ backallup.py
@@ -13,9 +13,10 @@ def main():
         websites = json.loads(output)
         
         for website in websites:
-            domain = website['domain']
-            if domain:
-                print(f"Backing up {domain}")
+            print(website)
+            # domain = website['domain']
+            # if domain:
+            #     print(f"Backing up {domain}")
                 # os.system(f"python3 /root/auto_backup/cyberpanel_backup.py {domain}")
     except json.JSONDecodeError as e:
         print(f"Error parsing JSON: {e}")
